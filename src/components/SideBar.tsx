407d24599b380c7da405cc4421305f7f708a372a
Lucas Vitorino <lucas.vitorino@alterans.tech>
2022-10-18 15:47:54 -0300

    feat(*): add components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,47 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "./Content";
+import { Button } from "./Button";
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import "../styles/sidebar.scss";
+
+interface SideBarProps {
+  clickButtonCallback: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({
+  clickButtonCallback,
+  selectedGenreId,
+}: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  function handleSelectGenre(id: number) {
+    clickButtonCallback(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleSelectGenre(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
